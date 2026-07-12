4ef21bab487782ad07eb38f7d56805e4b9e992a9
PAKJAWIR <ameisrain@gmail.com>
2026-07-13 01:42:51 +0700

    Remove CSS Border

--- src/component/molecules/NavbarButton/ButtonMobile.jsx
+++ src/component/molecules/NavbarButton/ButtonMobile.jsx
@@ -43,7 +43,7 @@ function ButtonMobile() {
       gsap.set(".menu-items-container", { autoAlpha: 1 });
       gsap.set(".split-word", { yPercent: 120, opacity: 0 });
       gsap.set(".menu-icon", { opacity: 0, scale: 0.8 });
-      gsap.set(".menu-border", { scaleX: 0, transformOrigin: "left center" });
+      gsap.set(".menu-", { scaleX: 0, transformOrigin: "left center" });
       gsap.set(".js-menu-line", { rotate: -75 });
 
       tl = gsap.timeline({
@@ -64,11 +64,11 @@ function ButtonMobile() {
         height: "16.5rem",
         duration: 1.4,
         ease: "expo.inOut",
-        borderRadius: "5%",
+        Radius: "5%",
       })
         .to(".js-menu-backdrop", { opacity: 1, pointerEvents: "auto", duration: 0.8, ease: "power2.out" }, "<")
         .to(".js-menu-line", { rotate: 0, duration: 0.8, ease: "power2.out" }, "<")
-        .to(".menu-border", { scaleX: 1, stagger: 0.08, duration: 0.8, ease: "power2.out" }, "-=0.5")
+        .to(".menu-", { scaleX: 1, stagger: 0.08, duration: 0.8, ease: "power2.out" }, "-=0.5")
         .to(".split-word", { yPercent: 0, opacity: 1, stagger: 0.08, duration: 0.8, ease: "power2.out" }, ">-=0.8")
         .to(".menu-icon", { opacity: 1, scale: 1, stagger: 0.08, duration: 0.8, ease: "power2.out" }, "<")
         .set(".menu-items-container a", { pointerEvents: "auto" });
@@ -147,14 +147,14 @@ function ButtonMobile() {
         <div className="relative flex justify-between h-full w-full z-10">
           {/* Logo (Jalur Utama ke Home) */}
           <div className="flex h-full w-full overflow-hidden p-6">
-            <div className="flex h-full w-full overflow-hidden border">
-              <TransitionLink to="/" className="js-global-logo-nav flex h-full w-fit border overflow-hidden ">
+            <div className="flex h-full w-full overflow-hidden ">
+              <TransitionLink to="/" className="js-global-logo-nav flex h-full w-fit  overflow-hidden ">
                 <Logo containerWidth="w-fit" containerHeight="h-full" className="overflow-hidden" />
               </TransitionLink>
             </div>
           </div>
           <div className="flex h-full w-full p-6">
-            <div className="js-navbar-btn flex items-center justify-end h-full w-full border">
+            <div className="js-navbar-btn flex items-center justify-end h-full w-full ">
               <button className="relative flex items-center justify-center h-full w-[14svw] rounded-sm ">
                 {/* Menu Wrapper */}
                 <div className="js-menu-wrapper bg-warna3 h-full w-full absolute top-0 right-0 z-0 overflow-hidden">
