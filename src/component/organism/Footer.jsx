94fb1ee75a918f312b4d56ee689a0e5556a3ee2c
PAKJAWIR <ameisrain@gmail.com>
2026-07-13 07:10:16 +0700

    Small Footer Changes

--- src/component/organism/Footer.jsx
+++ src/component/organism/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <>
       {/* CONTAINER UTAMA*/}
-      <div className="bg-warna1 h-[58svh] md:h-[68svh] w-svw p-6 flex flex-col ">
+      <div className="bg-warna1 h-[60svh] md:h-[68svh] w-svw p-6 flex flex-col ">
         <div className="flex flex-col h-full w-full pt-6 justify-between border-t border-warna2/10 md:border-none">
           {/* TOP ROW*/}
           <div className="flex flex-col-reverse lg:flex-row justify-around md:justify-between items-start flex-1 w-full ">
@@ -14,7 +14,7 @@ function Footer() {
             </div>
 
             {/* Navigasi Area*/}
-            <div className="flex justify-start md:justify-end gap-12 flex-row w-full md:w-auto items-start ">
+            <div className="flex justify-start md:justify-end gap-12 flex-row h-full w-full md:w-auto items-start ">
               {/* Links */}
               <div className="flex flex-col gap-6 w-fit items-start">
                 <h3 className="text-warna2/45 uppercase text-md font-bold">Links</h3>
